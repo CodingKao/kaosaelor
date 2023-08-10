7b8db5b7416b7c46387af77e8425196d8fb9affa
Kao Saelor <saelor.kao@gmail.com>
2023-08-10 13:55:45 -0700

    emailjs added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import './contact.css';
 import { MdOutlineEmail } from 'react-icons/md';
 // import { RiMessengerLine } from 'react-icons/ri';
+import emailjs from 'emailjs-com';
+import { useRef } from 'react';
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_in0ob0t', 'template_xsfj3gt', form.current, 'vJFPuweGb_rfTPAMt')
+          
+        e.target.reset()
+      };
+
     return (
         <section id='contact'>
             <h5>Get In Touch</h5>
@@ -33,7 +45,7 @@ const Contact = () => {
                     </article> */}
 
                 </div>
-                <form action="">
+                <form ref={form} onSubmit={sendEmail}>
                     <input type="text" name='name' placeholder='Your Full Name' required></input>       
                     <input type="email" name='email' placeholder='Your Email' required></input>   
                     <textarea name="message" rows="7" placeholder="Your Message" required></textarea>          
